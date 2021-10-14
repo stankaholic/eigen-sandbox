4432741d85b3bed07450f69a4f5c27cbb383ecb6
Joshua Lynn <jawatlynn@gmail.com>
2021-10-13 21:59:45 -0500

    adding xcode proj and git ignore

---
+++ eigen-sandbox/eigen-sandbox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  eigen-sandbox
+//
+//  Created by Joshua Lynn on 10/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
