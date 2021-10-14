e347b8149aedd13bb5644e8131f1b850db071d11
Joshua Lynn <jawatlynn@gmail.com>
2021-10-13 22:27:32 -0500

    adding eigen code and beginner main

--- eigen-sandbox/eigen-sandbox/main.cpp
+++ eigen-sandbox/eigen-sandbox/main.cpp
@@ -1,14 +1,14 @@
-//
-//  main.cpp
-//  eigen-sandbox
-//
-//  Created by Joshua Lynn on 10/13/21.
-//
-
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include "Eigen/Dense"
+ 
+using Eigen::MatrixXd;
+ 
+int main(int argc, const char * argv[])
+{
+  MatrixXd m(2,2);
+  m(0,0) = 3;
+  m(1,0) = 2.5;
+  m(0,1) = -1;
+  m(1,1) = m(1,0) + m(0,1);
+  std::cout << m << std::endl;
 }
